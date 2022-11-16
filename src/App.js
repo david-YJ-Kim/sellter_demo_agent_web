d275a15e0cf755bd07775bfb66447c8b2150cf75
david-YJ-Kim <yj.davd.kim@gmail.com>
2022-11-17 08:44:50 +0900

    merging origin source

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { MemoryRouter as Router, Routes, Route } from "react-router-dom";
+import Hello from "../ui/routes/Hello";
+import Home from "../ui/routes/Home";
+import "./App.css";
+import Login from "./../ui/routes/LogIn";
+import BizAccount from "./../ui/routes/BizAccount";
+import SalesManagement from "./../ui/routes/SalesManagement";
+import KeywordCollection from "./../ui/routes/KeywordCollection​";
+import ItemDeploy from "./../ui/routes/ItemDeploy";
+import ItemCollection from "./../ui/routes/ItemCollection";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/bizAccount" element={<BizAccount />} />
+        <Route path="/itemCollection" element={<ItemCollection />} />
+        <Route path="/itemDeploy" element={<ItemDeploy />} />
+        <Route path="/keywordCollection" element={<KeywordCollection />} />
+        <Route path="/salesManagement" element={<SalesManagement />} />
+      </Routes>
+    </Router>
   );
 }
-
-export default App;
---
+++ src/routes/Hello.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import icon from "../static/icon.svg";
+import LoginOrAssignForm from "../component/molecules/BasicForm";
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        {/* <img width="200" alt="icon" src={icon} /> */}
+      </div>
+      <div className="Hello">
+        <LoginOrAssignForm></LoginOrAssignForm>
+      </div>
+    </div>
+  );
+};
+
+export default Hello;
